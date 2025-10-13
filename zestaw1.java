ae8781a2c939a6d2172c8aab6e6332919b0e0a9d
ELLVO3 <maciek160805@gmail.com>
2025-10-13 13:58:48 +0200

    Add files via upload

--- zestaw1.java
+++ zestaw1.java
@@ -16,7 +16,7 @@ public class zestaw1 {
 
     // Zadanie 0.
     // Napisz funkcję główną (main).
-    public static void main (String[] args){
+    public static void main(String[] args) {
         // zadanie_1();
         // zadanie_2();
         // zadanie_3();
@@ -30,20 +30,28 @@ public class zestaw1 {
         // zadanie_14();
         // System.out.println(zadanie_15());
         // zadanie_16();
-        zadanie_17();
+        // zadanie_17();
+        // zadanie_10();
+        // zadanie_11();
+        // zadanie_18();
+        // zadanie_19();
+        // System.out.println(zadanie_21(5));
+        // System.out.println(zadanie_22(5));
+        // System.out.println(zadanie_23(5, 3));
+        System.out.println(zadanie_30(30));
     }
 
     // Zadanie 1.
     // Wypisz swoje imię i nazwisko na ekranie.
 
-    static void zadanie_1(){
+    static void zadanie_1() {
         System.out.println("Maciej W");
     }
 
     // Zadanie 2.
     // Wypisz długość imienia i nazwiska.
 
-    static void zadanie_2(){
+    static void zadanie_2() {
         String firstName = "Maciej";
         String lastName = "Wojciechowski";
 
@@ -55,15 +63,15 @@ public class zestaw1 {
     // Za pomocą printf połącz 3 wyrazy "Ala" "ma" "kota"
     // w jeden i wyświetl go na ekranie.
 
-    static void zadanie_3(){
-        System.out.printf("%s %s %s","Ala","ma","kota");
+    static void zadanie_3() {
+        System.out.printf("%s %s %s", "Ala", "ma", "kota");
     }
 
     // Zadanie 4.
     // W zdaniu „Ala ma kota” zastąp
     // wszystkie litery „a” literą „e”.
 
-    static void zadanie_4(){
+    static void zadanie_4() {
         String text = "Ala ma kota";
         System.out.println(text.replace("a", "e"));
     }
@@ -72,7 +80,7 @@ public class zestaw1 {
     // Napisz program, który będzie pobierał dwa słowa
     // od użytkownika i wypisywał je na ekranie.
 
-    static void zadanie_5(){
+    static void zadanie_5() {
         Scanner myObj = new Scanner(System.in);
         System.out.print("Podaj pierwsze słowo: ");
         String slowo1 = myObj.nextLine();
@@ -87,8 +95,8 @@ public class zestaw1 {
     // Napisz program, który będzie pobierał dwie liczby od
     // użytkownika i wypisał ich sumę, różnicę, iloczyn i iloraz.
 
-    static void zadanie_6(){
-        Scanner  sc = new Scanner(System.in);
+    static void zadanie_6() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Podaj pierwszą liczbę: ");
         float l1 = sc.nextFloat();
 
@@ -118,23 +126,23 @@ public class zestaw1 {
     //      • Przesuniętą o 5 bitów w lewo
     //      • Przesuniętą o 6 bitów w prawo
 
-    static void zadanie_8(){
+    static void zadanie_8() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Podaj liczbę: ");
         int x = sc.nextInt();
 
-        System.out.println(x+140);
-        System.out.println(x-31);
-        System.out.println(x*7);
-        System.out.println(x/13.0);
-        System.out.println(x%7);
-        System.out.println(x/4);
+        System.out.println(x + 140);
+        System.out.println(x - 31);
+        System.out.println(x * 7);
+        System.out.println(x / 13.0);
+        System.out.println(x % 7);
+        System.out.println(x / 4);
         System.out.println(Math.pow(x, 45));
-        System.out.println(x&67);
-        System.out.println(x|59);
-        System.out.println(x^23);
-        System.out.println(x<<5);
-        System.out.println(x>>6);
+        System.out.println(x & 67);
+        System.out.println(x | 59);
+        System.out.println(x ^ 23);
+        System.out.println(x << 5);
+        System.out.println(x >> 6);
     }
 
     // Zadanie 9.
@@ -159,39 +167,7 @@ public class zestaw1 {
     // Napisz program, który pobiera od użytkownika trzy liczby
     // i wypisuje je w kolejności rosnącej.
 
-    void zadanie_10(){
-
-    }
-
-    // Zadanie 11.
-    // Napisz program, który pobiera od użytkownika trzy liczby
-    // i wypisuje je w kolejności malejącej.
-
-    void zadanie_11(){
-
-    }
-
-    // Zadanie 12.
-    // Napisz program, który rozwiązuje równanie liniowe ax + b = 0
-    // o parametrach a i b podanych przez użytkownika.
-
-    static void zadanie_12(){
-            Scanner scan = new Scanner(System.in);
-
-            System.out.print("Podaj liczbę a: ");
-            float a = scan.nextFloat();
-
-            System.out.print("Podaj liczbę b: ");
-            float b = scan.nextFloat();
-
-            System.out.println("x = " + (-b/a));
-    }
-
-    // Zadanie 13.
-    // Napisz program, który pobiera od użytkownika wartości parametrów
-    // a, b i c, i sprawdza czy można z nich zbudować trójkąt.
-
-    static void zadanie_13(){
+    static void zadanie_10() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Podaj a: ");
@@ -203,11 +179,96 @@ public class zestaw1 {
         System.out.print("Podaj c: ");
         int c = sc.nextInt();
 
-        if(a + b > c && a + c > b && b + c > a){
-            System.out.println("Można zbudować trójkąt :)");
+        if (a > b) {
+            int t = a;
+            a = b;
+            b = t;
+        }
+        if (b > c) {
+            int t = b;
+            b = c;
+            c = t;
+        }
+        if (a > b) {
+            int t = a;
+            a = b;
+            b = t;
         }
 
-        else{
+        System.out.print(a + "" + b + "" + c);
+    }
+
+
+    // Zadanie 11.
+    // Napisz program, który pobiera od użytkownika trzy liczby
+    // i wypisuje je w kolejności malejącej.
+
+    static void zadanie_11() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Podaj a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Podaj b: ");
+        int b = sc.nextInt();
+
+        System.out.print("Podaj c: ");
+        int c = sc.nextInt();
+
+        if (a < b) {
+            int t = a;
+            a = b;
+            b = t;
+        }
+        if (b < c) {
+            int t = b;
+            b = c;
+            c = t;
+        }
+        if (a < b) {
+            int t = a;
+            a = b;
+            b = t;
+        }
+
+        System.out.print(a + "" + b + "" + c);
+    }
+
+    // Zadanie 12.
+    // Napisz program, który rozwiązuje równanie liniowe ax + b = 0
+    // o parametrach a i b podanych przez użytkownika.
+
+    static void zadanie_12() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę a: ");
+        float a = scan.nextFloat();
+
+        System.out.print("Podaj liczbę b: ");
+        float b = scan.nextFloat();
+
+        System.out.println("x = " + (-b / a));
+    }
+
+    // Zadanie 13.
+    // Napisz program, który pobiera od użytkownika wartości parametrów
+    // a, b i c, i sprawdza czy można z nich zbudować trójkąt.
+
+    static void zadanie_13() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Podaj a: ");
+        int a = sc.nextInt();
+
+        System.out.print("Podaj b: ");
+        int b = sc.nextInt();
+
+        System.out.print("Podaj c: ");
+        int c = sc.nextInt();
+
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("Można zbudować trójkąt :)");
+        } else {
             System.out.println("Nie można zbudować trójkąta :(");
         }
     }
@@ -217,7 +278,7 @@ public class zestaw1 {
     // a, b i c, które powinny być bokami pewnego trójkąta
     // i sprawdza czy trójkąt jest ostrokątny, prostokątny czy rozwartokątny.
 
-    static void zadanie_14(){
+    static void zadanie_14() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Podaj a: ");
@@ -229,19 +290,15 @@ public class zestaw1 {
         System.out.print("Podaj c: ");
         int c = sc.nextInt();
 
-        if(a + b > c && a + c > b && b + c > a){
-            if(a*a + b*b > c*c){
+        if (a + b > c && a + c > b && b + c > a) {
+            if (a * a + b * b > c * c) {
                 System.out.println("Trójkąt jest ostrokątny :>");
-            }
-            else if(a*a + b*b == c*c){
+            } else if (a * a + b * b == c * c) {
                 System.out.println("Trójkąt jest prostokątny :L");
-            }
-            else{
+            } else {
                 System.out.println("Trójkąt jest rozwartokątny :>>");
             }
-        }
-
-        else{
+        } else {
             System.out.println("Nie można zbudować trójkąta :(");
         }
     }
@@ -250,7 +307,7 @@ public class zestaw1 {
     // Napisz program, który pobiera od użytkownika liczbę m oraz n
     // i zwraca resztę z dzielenia m przez n.
 
-    static int zadanie_15(){
+    static int zadanie_15() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Podaj liczbę m: ");
@@ -272,26 +329,20 @@ public class zestaw1 {
     //      Danina solidarnościowa: od 1 000 000 zł stawka 4%
     // Kwota zmniejszająca podatek wynosi 3 600 zł.
 
-    static void zadanie_16(){
+    static void zadanie_16() {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Podaj dochód: ");
         float dochod = sc.nextFloat();
 
-        if(dochod < 30000.00){
+        if (dochod < 30000.00) {
             System.out.println("Brak podatku należnego");
-        }
-
-        else if(dochod > 30000.00 && dochod < 120000.00){
+        } else if (dochod > 30000.00 && dochod < 120000.00) {
             System.out.println("Podatek należny: " + ((dochod * 0.12) - 3600.00));
-        }
-
-        else if(dochod < 120000.00 && dochod > 1000000.00){
+        } else if (dochod < 120000.00 && dochod > 1000000.00) {
             System.out.println("Podatek należny: " + ((dochod * 0.32) - 3600.00));
-        }
-
-        else{
+        } else {
             System.out.println("Podatek należny: " + (dochod * 0.04));
         }
 
@@ -302,17 +353,17 @@ public class zestaw1 {
     // i wypisze dla długości boków od 1 do n
     // wszystkie trójki pitagorejskie.
 
-    static void zadanie_17(){
+    static void zadanie_17() {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Podaj liczbę n: ");
         int n = sc.nextInt();
 
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                for(int k = 1; k <= n; k++){
-                    if(i*i + j*j == k*k && i < j && j < k){
-                        System.out.printf("%d %d %d\n", i,j,k);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= n; k++) {
+                    if (i * i + j * j == k * k && i < j && j < k) {
+                        System.out.printf("%d %d %d\n", i, j, k);
                     }
                 }
             }
@@ -323,8 +374,25 @@ public class zestaw1 {
     // Napisz program, który pobiera od użytkownika liczbę n
     // i wypisze wszystkie liczby pierwsze od 2 do n.
 
-    void zadanie_18(){
+    static void zadanie_18() {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Podaj n: ");
+
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            boolean flaga = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    flaga = false;
+                    break;
+                }
+            }
+            if (flaga) {
+                System.out.println(i);
+            }
+        }
     }
 
     // Zadanie 19.
@@ -332,7 +400,22 @@ public class zestaw1 {
     // a następnie wypisze wszystkie liczby m-cyfrowe,
     // które podzielne są przez n.
 
-    void zadanie_19(){
+    static void zadanie_19() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Podaj m: ");
+        int m = sc.nextInt();
+
+        System.out.print("Podaj n: ");
+        int n = sc.nextInt();
+
+        int start = (int) Math.pow(10, m - 1);
+        int end = (int) Math.pow(10, m) - 1;
+
+        for (int i = start; i <= end; i++) {
+            if (i % n == 0) {
+                System.out.println(i);
+            }
+        }
 
     }
 
@@ -341,7 +424,7 @@ public class zestaw1 {
     // w dwóch wariantach, której wysokość jest liczbą n.
     // Program powinien przyjmować dwa argumenty - n (wysokość) i v (wariant).
 
-    void zadanie_20(){
+    void zadanie_20() {
 
     }
 
@@ -349,24 +432,42 @@ public class zestaw1 {
     // Napisz program, który obliczy i zwróci silnie liczby n.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_21(){
+    static int zadanie_21(int n) {
+        int wynik = 1;
 
+        for (int i = n; i > 1; i--) {
+            wynik *= i;
+        }
+
+        return wynik;
     }
 
     // Zadanie 22.
     // Napisz program, który obliczy i zwróci silnie podwójną liczby n.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_22(){
+    static int zadanie_22(int n) {
+        int wynik = 1;
 
+        for (int i = n; i > 1; i -= 2) {
+            wynik *= i;
+        }
+
+        return wynik;
     }
 
     // Zadanie 23.
     // Napisz program, który obliczy i zwróci silnie m-tą liczby n.
     // Program powinien przyjmować dwa argumenty - liczbę m oraz n.
 
-    void zadanie_23(){
+    static int zadanie_23(int n, int m) {
+        int wynik = 1;
 
+        for (int i = n; i > 1; i -= m) {
+            wynik *= i;
+        }
+
+        return wynik;
     }
 
     // Zadanie 24.
@@ -374,7 +475,7 @@ public class zestaw1 {
     // kolejnych liczb naturalnych.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_24(){
+    void zadanie_24() {
 
     }
 
@@ -383,7 +484,7 @@ public class zestaw1 {
     // kolejnych liczb parzystych.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_25(){
+    void zadanie_25() {
 
     }
 
@@ -392,7 +493,7 @@ public class zestaw1 {
     // kolejnych liczb nieparzystych.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_26(){
+    void zadanie_26() {
 
     }
 
@@ -401,7 +502,7 @@ public class zestaw1 {
     // liczb będących kwadratami kolejnych liczb naturalnych.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_27(){
+    void zadanie_27() {
 
     }
 
@@ -410,7 +511,7 @@ public class zestaw1 {
     // liczb będących sześcianami kolejnych liczb naturalnych.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_28(){
+    void zadanie_28() {
 
     }
 
@@ -419,7 +520,7 @@ public class zestaw1 {
     // liczb będących odwrotnościami kolejnych liczb naturalnych.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_29(){
+    void zadanie_29() {
 
     }
 
@@ -428,7 +529,13 @@ public class zestaw1 {
     // czy liczba n jest doskonała.
     // Program powinien przyjmować jeden argument - liczbę n.
 
-    void zadanie_30(){
-
+    static boolean zadanie_30(int n) {
+        int suma = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                suma += i;
+            }
+        }
+        return n == suma;
     }
 }
